6b7228182833a25e11755b192d3339193530c285
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-03-12 07:56:30 -0500

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,26 +5,23 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
 
-
-  def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
-        if !category_attribute[:name].empty? 
-          category = Category.find_or_create_by(category_attribute)
-          self.categories << category
-        end
-    end
-  end
-
-  def unique_users_who_commented
-    unique_user = []
-
-    self.comments.each do |comment| 
-      if !unique_user.include?(comment.user) 
-        unique_user << comment.user
+  def categories_attributes=(category_attribute)
+    category_attribute.values.each do |category_attribute|
+      if category_attribute["name"].present?
+       category = Category.find_or_create_by(category_attribute)
+        self.categories << category
       end
     end
-  
-    unique_user
   end
 
+  def unique_users
+    user_list = []
+
+    self.users.each do |user| 
+      if !user_list.include?(user)
+       user_list << user 
+      end
+    end
+    user_list
+  end
 end
